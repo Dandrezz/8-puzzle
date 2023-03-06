5f57f99a821f1b204939cfaa9786046894960804
Diego Andrés Portero López <dportero@montran.com>
2023-03-05 21:22:41 -0500

    Implementación de funcionalidad v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,131 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
 
-	const validKeys = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight','w','a','s','d']
+	const validKeys = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'w', 'a', 's', 'd']
 
-	const handlePressKey = (e: KeyboardEvent) => {
-		if (validKeys.includes(e.key)){
-			console.table(e.key);
+	const stateGame = {
+		1: {
+			x: 0,
+			y: 0,
+		},
+		2: {
+			x: 0,
+			y: 1,
+		},
+		3: {
+			x: 0,
+			y: 2,
+		},
+		4: {
+			x: 1,
+			y: 0,
+		},
+		5: {
+			x: 1,
+			y: 1,
+		},
+		6: {
+			x: 1,
+			y: 2,
+		},
+		7: {
+			x: 2,
+			y: 0,
+		},
+		8: {
+			x: 2,
+			y: 1,
+		},
+		0: {
+			x: 2,
+			y: 2,
 		}
 	}
+
+	const dataState: number[][] = [
+		[1, 2, 3],
+		[4, 5, 6],
+		[7, 8, 0]
+	]
+
+	const handlePressKey = (e: KeyboardEvent) => {
+		if (validKeys.includes(e.key)) {
+			const { x, y } = stateGame[0]
+			let target = -1;
+			if (e.key === 'ArrowUp' || e.key === 'w') {
+				const newX = x + 1
+				if (newX < 3) {
+					target = dataState[newX][y]
+					const temp = dataState[x][y]
+					dataState[x][y] = dataState[newX][y]
+					dataState[newX][y] = temp
+					stateGame[target].x = stateGame[0].x
+					stateGame[0].x = newX
+				}
+			} else if (e.key === 'ArrowDown' || e.key === 's') {
+				const newX = x - 1
+				if (newX >= 0) {
+					target = dataState[newX][y]
+					const temp = dataState[x][y]
+					dataState[x][y] = dataState[newX][y]
+					dataState[newX][y] = temp
+					stateGame[target].x = stateGame[0].x
+					stateGame[0].x = newX
+				}
+			} else if (e.key === 'ArrowLeft' || e.key === 'a') {
+				const newY = y + 1
+				if (newY < 3) {
+					target = dataState[x][newY]
+					const temp = dataState[x][y]
+					dataState[x][y] = dataState[x][newY]
+					dataState[x][newY] = temp
+					stateGame[target].y = stateGame[0].y
+					stateGame[0].y = newY
+				}
+			} else if (e.key === 'ArrowRight' || e.key === 'd') {
+				const newY = y - 1
+				if (newY >= 0){
+					target = dataState[x][newY]
+					const temp = dataState[x][y]
+					dataState[x][y] = dataState[x][newY]
+					dataState[x][newY] = temp
+					stateGame[target].y = stateGame[0].y
+					stateGame[0].y = newY
+				}
+			}
+			setDataTable(dataState.flat())
+		}
+	}
+
+	const [dataTable, setDataTable] = useState(dataState.flat())
 
 	useEffect(() => {
 		document.addEventListener('keydown', handlePressKey)
 	}, [])
-	
 
 	return (
-		<div 
-			className="max-w-xl m-auto grid grid-cols-1 place-content-center w-full h-screen p-4 text-center" 
-			style={{backgroundColor:'#FFB703'}}
-			onKeyDown={(e)=>console.log(e.target)}>
-			<h1 className='text-6xl text-white mb-5'>8 puzzle</h1>
-			<div className='grid grid-cols-3 gap-4 p-5 rounded-lg justify-items-stretch w-96 h-96 mx-auto' style={{backgroundColor:'#023047'}}>
-				<div className='section'>1</div>
-				<div className='section'>2</div>
-				<div className='section'>3</div>
-				<div className='section'>4</div>
-				<div className='section'>5</div>
-				<div className='section'>6</div>
-				<div className='section'>7</div>
-				<div></div>
-				<div className='section'>8</div>
+		<div
+			className="max-w-xl m-auto grid grid-cols-1 place-content-center w-full h-screen p-4 text-center"
+			style={{ backgroundColor: '#FFB703' }}>
+			<h1 className='text-6xl text-white mb-5'>8-Puzzle Game</h1>
+			<div className='grid grid-cols-3 gap-4 p-5 rounded-lg justify-items-stretch w-96 h-96 mx-auto' style={{ backgroundColor: '#023047' }}>
+				{
+					dataTable.map((item) => {
+						return (
+							item !== 0 ?
+								<div
+									key={item}
+									className='section text-5xl text-white rounded-lg align-middle'>
+									{item}
+								</div> :
+								<div key={item}></div>
+						)
+					})
+				}
 			</div>
+			<button className="text-white rounded-lg section mt-5 w-72 mx-auto text-2xl h-12">Shuffle</button>
 		</div>
 	)
 }
